0946d297e98b76385a531ee8cea153e97eff0fc4
smtnhacker <themikosurara@gmail.com>
2023-05-18 16:19:09 +0800

    feat: add device post query

--- server/index.js
+++ server/index.js
@@ -19,6 +19,49 @@ app.get('/api/user_input/all', async (req, res) => {
     }
 })
 
+// actual APIs
+app.post('/api/device_response', async (req, res) => {
+    try {
+        const { kwh, device_id } = req.body;
+        // scripted muna device_id = 1
+        // scripted muna user_id = 1
+        const { user_id } = (await db.query('SELECT user_id FROM device_user WHERE device_id = $1', [device_id]))[0];
+        const user_info = (await db.query('SELECT * FROM user_input WHERE id = $1', [user_id]))[0];
+        const deviceQuery = `INSERT INTO device_response (device_id, created_on, current_value, user_id)
+                                VALUES ($1, NOW(), $2, 1) RETURNING *`;
+
+        const estimatedCost = kwh * user_info.cost_per_kwh;
+        let led_status;
+        let buzzer_status;
+
+        if (estimatedCost < user_info.thresh_low) {
+            led_status = 0;
+            buzzer_status = 0;
+        } else if (estimatedCost < user_info.thresh_up) {
+            led_status = 1;
+            buzzer_status = 1;
+        } else {
+            led_status = 2;
+            buzzer_status = 2;
+        }
+        
+        const device = (await db.query(deviceQuery, [device_id, kwh]))[0];
+        const responseQuery = `INSERT INTO cloud_response 
+                                (device_id, created_on, estimated_cost, led_status, buzzer_status, device_response_id)
+                            VALUES ($1, NOW(), $2, $3, $4, $5) RETURNING *`;
+        const cloudResponse = (await db.query(responseQuery, [device_id, estimatedCost, led_status, buzzer_status, device.id]))[0];
+
+        return res.json({
+            cost: cloudResponse.estimated_cost,
+            led_status: cloudResponse.led_status,
+            buzzer_status: cloudResponse.buzzer_status
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Something went wrong!')
+    }
+})
+
 // Health test APIs
 app.get('/api/up', (req, res) => {
     res.send('Backend is running!');
--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,19 +1,3 @@
-CREATE TABLE device_response (
-    id SERIAL PRIMARY KEY,
-    device_id INT NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-    current_value FLOAT8 NOT NULL
-);
-
-CREATE TABLE cloud_response (
-    id SERIAL PRIMARY KEY,
-    device_id INT NOT NULL,
-    created_on TIMESTAMP NOT NULL, 
-    estimated_cost FLOAT8 NOT NULL,
-    led_status INT NOT NULL,
-    buzzer_status INT NOT NULL
-);
-
 CREATE TABLE user_input (
     id SERIAL PRIMARY KEY,
     cost_per_kwh FLOAT8 NOT NULL,
@@ -25,4 +9,27 @@ CREATE TABLE user_auth (
     id SERIAL PRIMARY KEY,
     username VARCHAR(64),
     passplain VARCHAR(64)
+);
+
+CREATE TABLE device_user (
+    device_id INT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL REFERENCES user_input(id)
+);
+
+CREATE TABLE device_response (
+    id SERIAL PRIMARY KEY,
+    device_id INT NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    current_value FLOAT8 NOT NULL,
+    user_id INT NOT NULL FOREIGN KEY REFERENCES user_input(id)
+);
+
+CREATE TABLE cloud_response (
+    id SERIAL PRIMARY KEY,
+    device_id INT NOT NULL,
+    created_on TIMESTAMP NOT NULL, 
+    estimated_cost FLOAT8 NOT NULL,
+    led_status INT NOT NULL,
+    buzzer_status INT NOT NULL,
+    device_response_id FOREIGN KEY REFERENCES device_response(id)
 );
