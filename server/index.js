c2ba841c73c861df2e07072094c97fd41d845db0
smtnhacker <themikosurara@gmail.com>
2023-05-22 16:48:59 +0800

    fix (server): add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const db = require('./util/db');
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
